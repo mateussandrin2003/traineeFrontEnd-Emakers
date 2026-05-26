f08683a508b6c7b3954abe737fe1df0801c3c030
Mateus Sandrin <mateus.sandrin1@estudante.ufla.br>
2026-05-26 20:06:51 -0300

    Inserção do botão de Cadastrar-se, Entrar e  Precisa de ajuda, além de hyperlinks nos mesmos. Há validação de e-mail também.

--- trainee-Emakers/src/PaginaDeLogin.jsx
+++ trainee-Emakers/src/PaginaDeLogin.jsx
@@ -2,15 +2,27 @@ import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash, faMessage } from "@fortawesome/free-regular-svg-icons";
 import "./StyleLogin.css";
-import logo from "./Imagens/Logo.png";  
+import logo from "./Imagens/Logo.png";
 
 export default function PaginaDeLogin() {
   const [mostrarSenha, setMostrarSenha] = useState(false);
+  const [email, setEmail] = useState("");
+  const [emailValido, setEmailValido] = useState(true);
+
+  function handleEntrar() {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    setEmailValido(regex.test(email));
+  }
 
   return (
     <>
       {/* Logo */}
-      <img src={logo} alt="Logo" className="logo" />
+      <a href="https://www.google.com/">
+        <img src={logo} alt="Logo" className="logo" />
+      </a>
+
+      {/* Link Cadastre-se */}
+      <a href="https://www.google.com/" className="link-cadastro">Cadastre-se agora</a>
 
       {/* Textos de login */}
       <h1 className="titulo">Login</h1>
@@ -26,7 +38,16 @@ export default function PaginaDeLogin() {
             <label htmlFor="email" className="texto-campo">
               Endereço de e-mail
             </label>
-            <input type="email" id="email" className="input-login" />
+            <input
+              type="email"
+              id="email"
+              className={`input-login ${!emailValido ? "input-erro" : ""}`}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {!emailValido && (
+              <span className="mensagem-erro">E-mail inválido ou inexistente.</span>
+            )}
           </div>
 
           <div className="grupo-campo">
@@ -52,8 +73,14 @@ export default function PaginaDeLogin() {
             Esqueceu sua senha?
           </a>
 
+          {/* Botão Entrar */}
+          <button className="botao-entrar" onClick={handleEntrar}>Entrar</button>
+
         </div>
       </div>
+
+      {/* Texto de ajuda */}
+      <a href="https://www.google.com/" className="texto-ajuda">Precisa de ajuda para entrar?</a>
 
       {/* Ícone de mensagem */}
       <div className="circulo-mensagem">
